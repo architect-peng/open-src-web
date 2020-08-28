a92954389fe3194051961e99d0aa66bbd3e613d2
15718852195@163.com <6552393peng>
2020-08-28 18:06:56 +0800

    组件开始

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-row>
+      <a-col :span="12">
+        col-12
+      </a-col>
+      <a-col :span="12">
+        col-12
+      </a-col>
+    </a-row>
+    <a-row>
+      <a-col :span="8">
+        col-8
+      </a-col>
+      <a-col :span="8">
+        col-8
+      </a-col>
+      <a-col :span="8">
+        col-8
+      </a-col>
+    </a-row>
+    <a-row>
+      <a-col :span="6">
+        col-6
+      </a-col>
+      <a-col :span="6">
+        col-6
+      </a-col>
+      <a-col :span="6">
+        col-6
+      </a-col>
+      <a-col :span="6">
+        col-6
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { Button } from 'ant-design-vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+
   }
 }
 </script>
